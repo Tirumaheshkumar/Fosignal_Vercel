5e6bd950098a673b6bbaa0068da65a6541ce7579
Mahi <tirumaheshkumar@gmail.com>
2026-03-24 15:30:32 +0530

    Update upstox-token.js

--- api/upstox-token.js
+++ api/upstox-token.js
@@ -1,3 +1,6 @@
+export const config = {
+  runtime: 'nodejs'
+};
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
